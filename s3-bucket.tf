c6addf948f13c3521f78f367980b4eaed9662e9f
Robert McLeod <robert.mcleod@phocassoftware.com>
2023-02-22 02:07:10 +0000

    Add s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+module "s3-bucket" {
+  source  = "app.terraform.io/robert-mcleod-phocas/s3-bucket/aws"
+  version = "2.8.0"
 
-  bucket = "databases"
   acl    = "private"
 
   bucket_prefix = var.prefix
@@ -9,5 +9,4 @@ module "s3_bucket" {
   versioning = {
     enabled = true
   }
-
 }
